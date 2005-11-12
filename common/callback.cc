c98b4d73247a8a503278673d80340e07eafda0f0
Sam Steele <sam.steele@gmail.com>
2005-11-12 18:32:34 +0000

    Code cleanup

--- common/callback.cc
+++ common/callback.cc
@@ -16,6 +16,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
+#include <Tiki/tiki.h>
+#include <Tiki/texture.h>
+
+using namespace Tiki;
+using namespace Tiki::GL;
 
 #include <string.h>
 #include <stdlib.h>
