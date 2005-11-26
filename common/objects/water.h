35de5327c87a9e649fcceb0b38f7cc687c8aff6f
Sam Steele <sam.steele@gmail.com>
2005-11-26 03:01:37 +0000

    Disable powerups

--- common/objects/water.h
+++ common/objects/water.h
@@ -1,19 +1,3 @@
-/* water.h - DCBlap water object
- * Copyright (c) 2001-2005 Sam Steele
- *
- * This file is part of DCBlap.
- *
- * DCBlap is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
- *
- * DCBlap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
- */
 void water_create(struct entity *me);
+void water_update(struct entity *me);
+void glass_create(struct entity *me);
