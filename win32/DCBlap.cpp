1f92aa3d1a045bfded7e658930587eb0406b90a8
Sam Steele <sam.steele@gmail.com>
2005-11-27 01:41:25 +0000

    Copyright update

--- win32/DCBlap.cpp
+++ win32/DCBlap.cpp
@@ -1,10 +1,20 @@
-/*
- *  TikiTest.cpp
- *  TikiTest
+/* DCBlap.cpp - Windows main
+ * Copyright (c) 2001-2005 Sam Steele
  *
- *  Created by Brian Peek on 1/23/05.
- *  Copyright 2005 Cryptic Allusion, LLC. All rights reserved.
+ * This file is part of DCBlap.
  *
+ * DCBlap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * DCBlap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
 #include "pch.h"
